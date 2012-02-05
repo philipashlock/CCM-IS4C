b968c73d3695f97a5b54b036baa758c45725d9c7
Philip Ashlock <phil@ashlock.us>
2012-02-05 15:46:23 -0500

    Updating price history table to avoid unique key conflicts

--- pos/fannie/updates/table.new.sql
+++ pos/fannie/updates/table.new.sql
@@ -3,8 +3,7 @@ create table products_price_history
   id int(11) not null, 
   upc bigint(13) unsigned zerofill not null, 
   updated timestamp default now(), 
-  normal_cost double default '0.00', 
-  primary key (id,upc) 
+  normal_cost double default '0.00'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1; 
 
 create table brands 
